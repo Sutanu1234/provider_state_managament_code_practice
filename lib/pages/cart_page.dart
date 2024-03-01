053cb85e7e994695b79ed138279071afb1472681
sutanu-bera <berasutanu02@gmail.com>
2024-03-02 03:13:33 +0530

    Add files via upload

---
+++ lib/pages/cart_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_state_managament_tutorial_yt/providers/cart_provider.dart';
+
+import '../consts.dart';
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appBar(),
+      body: _buildUI(),
+    );
+  }
+
+  PreferredSizeWidget _appBar() {
+    return AppBar(
+      title: const Text(
+        "Cart",
+      ),
+    );
+  }
+
+  Widget _buildUI() {
+    return Consumer<CartProvider>(
+      builder: (context, provider, _) {
+        return Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.sizeOf(context).height * 0.80,
+              child: ListView.builder(
+                itemCount: provider.items.length,
+                itemBuilder: (context, index) {
+                  Product product = provider.items[index];
+                  return ListTile(
+                    title: Text(
+                      product.name,
+                    ),
+                    onLongPress: () {
+                      provider.remove(product);
+                    },
+                  );
+                },
+              ),
+            ),
+            Text("Cart Total: \$${provider.getCartTotal()}")
+          ],
+        );
+      },
+    );
+  }
+}
